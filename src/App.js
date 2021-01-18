2fd64c8ed8800429e7d03f8f8890645243036f1d
ayasolaiman <ayasolaiman99@gmail.com>
2021-01-18 04:08:31 +0200

    Login Page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React from "react";
+import {
+  Router,
+  IndexRoute,
+  Route,
+  HomeRoute,
+  browserHistory
+} from "react-router";
+
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import LoginPage from "./pages/login/LoginForm";
 import "./style.css";
 
 export default function App() {
   return (
-    <div class="container">
-      <div class="header">
+    <div className="container">
+      <div className="header">
         <Header />
       </div>
-      <main />
-      <div class="footer">
+      <main>
+        <Router history={browserHistory}>
+          <HomeRoute path="/" component={LoginPage} />
+        </Router>
+      </main>
+      <div className="footer">
         <Footer />
       </div>
     </div>
